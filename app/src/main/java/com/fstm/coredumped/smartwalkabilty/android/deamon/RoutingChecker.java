521e86194039932855cc9329d2b616d20938b9b5
omarbelaqziz <omar.belaqziz@gmail.com>
2022-04-01 11:22:10 +0000

    OB: add RoutingChecker

--- app/src/main/java/com/fstm/coredumped/smartwalkabilty/android/deamon/RoutingChecker.java
+++ app/src/main/java/com/fstm/coredumped/smartwalkabilty/android/deamon/RoutingChecker.java
@@ -1,4 +1,21 @@
 package com.fstm.coredumped.smartwalkabilty.android.deamon;
 
-public class RoutingChecker {
+import com.fstm.coredumped.smartwalkabilty.android.ClientSocket;
+import com.fstm.coredumped.smartwalkabilty.android.model.bo.UserInfos;
+
+public class RoutingChecker extends Thread{
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(180000);
+            while(true){
+                Thread.sleep(300000);
+                if (!UserInfos.getInstance().isRouting()){
+                    new ClientSocket().SendAnnoncesReq(UserInfos.getInstance().getCurrentLocation());
+                }
+            }
+        } catch (InterruptedException e) {
+            return;
+        }
+    }
 }
